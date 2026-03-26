b469722f5f3f76321282d1cde97a08a68b2c30a0
ianq1w1 <ianpereiraian2@gmail.com>
2026-03-26 20:25:03 -0300

    adicionando HasAPItokens pra uso previo em testes de relações como API se utilizando da autenticação por token

--- ControlAR-app/app/Models/User.php
+++ ControlAR-app/app/Models/User.php
@@ -17,7 +17,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, TwoFactorAuthenticatable;
+    use HasFactory, Notifiable, TwoFactorAuthenticatable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
